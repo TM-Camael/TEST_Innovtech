a0aec5482d55d7737422b14f2a21fbcfb8139b48
Camael <tom.maillard.ts@gmail.com>
2020-06-29 17:22:35 +0200

    Ajoutez des fichiers projet.

---
+++ TEST_Innovtech/Services/TimerData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST_Innovtech.Services
+{
+    public class TimerData
+    {
+        public int Time { get; set; }
+    }
+}
